80e58f95b4314d53812455641abd1c2f5aa82de2
psalcedodev <psalcedodev@gmail.com>
2020-12-22 11:40:18 -0700

    oneToMany done

--- onetomany/routes/web.php
+++ onetomany/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Post;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/insert', function(){
+    $user = User::findOrFail(1);
+
+    $post = new Post(["title"=>"PHP with Paul", "body"=>"New content with PHP 3"]);
+    
+    $user->posts()->save($post);
+});
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+
+    foreach($user->posts as $post){
+        echo $post->body."<br>";
+    }
+    // return dd($user->posts);
+});
+
+Route::get("/update", function(){
+    $user = User::findOrFail(1);
+    $user->posts()->where("id", 1)->update(["title"=>"Hello 1", "body"=>"New content update!"]);
+});
+
+Route::get("/delete", function(){
+    $user = User::findOrFail(1);
+    $user->posts()->where("id", 1)->delete();
 });
--- programone/app/Video.php
+++ programone/app/Video.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     //
-    public function 
+
 }
